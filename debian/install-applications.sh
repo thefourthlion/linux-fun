3ed8b7d1075ae59895308c56a6f10ca8bdbdb4f2
snowflake <snowflake@localhost.localdomain>
2024-07-04 22:50:26 -0700

    add pia vpn to app list

--- debian/install-applications.sh
+++ debian/install-applications.sh
@@ -1,7 +1,7 @@
 \#!/bin/bash
 
 # List of packages to install
-packages=(
+apt_packages=(
     python3
     wireshark
     audacity
@@ -9,68 +9,74 @@ packages=(
     putty
     snapd
     curl
-    steam
-    # wine64
+    gh
+    wine64
+    brave-browsers
+    obs-studio
+    inkscape
+    net-tools
+    # local deb files
+    ./steam.deb 
+    ./codium_1.90.2.24171_amd64.deb
+    # docker
+    ca-certificates 
+    docker-ce 
+    docker-ce-cli 
+    containerd.io 
+    docker-buildx-plugin 
+    docker-compose-plugin
 )
 
-# Update package list
+snap_packages=(  
+  winbox
+  discord
+)
+
 sudo apt update
 
-# Install each package in the array
-for package in "${packages[@]}"
-do
-    sudo apt install -y "$package"
-done
+# ----- PIA VPN install 
+wget https://installers.privateinternetaccess.com/download/pia-linux-3.5.7-08120.run
 
-# ----- Discord install
-# sudo snap install discord
+sudo sh ./pia-linux-3.5.7-08120.run 
+
+# ----- Steam install
+wget https://steamcdn-a.akamaihd.net/client/installer/steam.deb
+
+# ----- VSCodium install
+wget https://code.visualstudio.com/docs/?dv=linux64_deb
 
 # ----- Brave install
-
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
-sudo apt update
-
-sudo apt install brave-browser
 
 # ----- OBS install
 sudo add-apt-repository ppa:obsproject/obs-studio
 
-sudo apt install obs-studio
-
-# ----- Winbox install 
-# sudo snap install winbox
-
 # ----- Inkscape install
 sudo add-apt-repository universe
 sudo add-apt-repository ppa:inkscape.dev/stable
-sudo apt-get update
-sudo apt install inkscape
 
 # ----- Docker Install
-
-# uninstall any conflicting packages
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-
-
-# Set up Docker's apt repository
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-
 # Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
 
-# install the latest version
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Install each apt package in the array
+for package in "${apt_packages[@]}"
+do
+    sudo apt install -y "$package"
+done
 
-docker -v
+# Install each snap package in the array
+for package in "${apt_packages[@]}"
+do
+    sudo snap install -y "$package"
+done
+
+echo "Applications done installing. Recomend doing a reboot.
